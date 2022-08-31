5ad195403de369cbd7edaa15639e043c05fbbfb8
DerMatte <me@dermatte.de>
2022-08-31 23:29:59 +0200

    rm Users, add _document, upgrade

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,23 @@
 import Link from "next/link";
+import cn from "classnames";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <nav className="flex space-x-4 py-2 sm:justify-center">
       {[
         ["Home", "/"],
         ["About", "/about"],
-        ["Users List", "/users"],
-        ["Users API", "/api/users"],
       ].map(([title, url]) => (
         <Link href={url} key={title}>
-          <a className="rounded-lg px-3 py-2 font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+          <a
+            className={cn(
+              router.asPath == url ? "underline underline-offset-2" : "",
+              "rounded-lg px-3 py-2 font-medium text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            )}
+          >
             {title}
           </a>
         </Link>
